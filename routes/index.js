99cac73f39183761311df87a757251354dfa4e09
juliwithoutthee <jnboucree@gmail.com>
2022-05-09 15:58:52 +0900

    Updates index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Just Desserts' });
+  res.send("Hi")
 });
+router.get('/item', async (req, res) => {
+  const items = await prisma.item.findMany()
+  res.json(items)
+})
 
 module.exports = router;
