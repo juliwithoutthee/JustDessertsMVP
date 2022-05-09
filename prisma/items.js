77f033fb4c2959d3c792556928692b40a36eab45
juliwithoutthee <jnboucree@gmail.com>
2022-05-09 09:54:37 +0900

    Removes image column for Item table

--- prisma/items.js
+++ prisma/items.js
@@ -4,34 +4,34 @@ const items = [
         itemName: "Cookies",
         description: "A dozen decadent cookies of your choice",
         price: 10, 
-        image:""
+ 
     }, 
     {
         id: 2,
         itemName: "Carrot Cake",
         description: "Topped with cream cheese frosting",
         price: 30, 
-        image:""
+
     }, 
     {
         id: 3,
         itemName: "Brownies",
         description: "Fudgy brownies made with quality chocolate",
         price: 15, 
-        image:""
+
     }, 
     {
         id: 4,
         itemName: "Apple Pie",
         description: "Made with fresh Fuji apples",
         price: 25, 
-        image:""
+
     }, 
     {
         id: 5,
         itemName: "Strawberry Shortcake",
         description: "Light, fluffy shortcake topped with whipped cream and fresh strawberries",
         price: 35, 
-        image:""
+
     }, 
 ]
