ce6b8aa68170f780e61e184069c9d4501ffd56f1
juliwithoutthee <jnboucree@gmail.com>
2022-05-09 16:18:36 +0900

    Removes var

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Just Desserts' });
   res.send("Hi")
 });
-router.get('/item', async (req, res) => {
+router.get('/items', async (req, res) => {
   const items = await prisma.item.findMany()
   res.json(items)
 })
