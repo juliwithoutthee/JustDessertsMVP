4dbaa40bb25f51ef2cc62b6b0e4e5aba5d97c1dc
juliwithoutthee <jnboucree@gmail.com>
2022-05-10 09:38:22 +0900

    Adds cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -17,7 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors());
 
 app.use('/', indexRouter); //api
 app.use('/users', usersRouter); //api/v1   
