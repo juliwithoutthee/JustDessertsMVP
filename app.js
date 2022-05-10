eb8acc04c0da725a531fdfbcf2aab092362d7ede
juliwithoutthee <jnboucree@gmail.com>
2022-05-10 10:23:58 +0900

    Cleans up code

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-const createError = require('http-errors');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(cors());
 
-app.use('/', indexRouter); //api
-app.use('/users', usersRouter); //api/v1   
+app.use('/', indexRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,9 +25,10 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ 
+    message: err.message,
+    error: err });
 });
 
 module.exports = app;
