d94bea6df5335a049ec6bf4228eca7c753207b28
juliwithoutthee <jnboucree@gmail.com>
2022-05-10 12:08:39 +0900

    Updates POST with correct prisma syntax

--- routes/index.js
+++ routes/index.js
@@ -13,21 +13,25 @@ router.get('/items', async (req, res) => {
   console.log(items);
   res.json(items);
 })
-router.post('/customers', async (req, res) => {
-  const result = await prisma.customer.create({
-  ...req.body,
-})
-res.json(result)
+router.post('/customer', async (req, res) => {
+  console.log(req.body, typeof req.body)
+  const { firstName, lastName, address, city, postalCode } = req.body;
+  const result = await prisma.customer.create(
+    { data: { firstName, lastName, address, city, postalCode } }
+  )
+  res.json(result)
 });
 
-router.get('/orders', async (req, res) =>{
-  //get order info from db
+router.get('/orders', async (req, res) => {
+  const orders = await prisma.order.findMany()
+  res.json(orders);
 });
 
 //Create new order
 router.post('/order', async (req, res) => {
+  const { customer, items, quantity, totalCost, timeOrdered } = req.body;
   const result = await prisma.order.create({
-    ...req.body,
+    data: { customer, items, quantity, totalCost, timeOrdered } 
   })
   res.json(result)
 });
